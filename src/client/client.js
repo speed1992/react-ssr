b2ee52d7ca8ecb68619b2754d5b385b215a9dac8
Paras Anand <speed1992@gmail.com>
2022-01-31 21:34:48 +0530

    client.js injected into the browser

---
+++ src/client/client.js
@@ -0,0 +1,4 @@
+// Startup point for a client side application
+
+alert("Hi!");
+console.log("Hi!");
--- webpack.client.js
+++ webpack.client.js
@@ -1,18 +1,15 @@
 const path = require('path');
 
 module.exports = {
-    // Inform webpack that we are building a bundle for Node JS, rather than the browser
-    target: 'node',
-
 
     // Tell webpack the root file of our server application
-    entry: './src/index.js',
+    entry: './src/client/client.js',
 
 
     // Tell webpack where to put the output file that is generated
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'public'),
     },
 
     // Tell webpack to run babel on every file it runs through
