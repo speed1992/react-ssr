72134afed038ad6599f1927cbbc801aa782b21b9
Paras Anand <speed1992@gmail.com>
2022-02-03 12:00:51 +0530

    redux store being sent to client now

--- src/client/client.js
+++ src/client/client.js
@@ -10,7 +10,7 @@ import thunk from "redux-thunk";
 import reducers from "./reducers";
 import Routes from "./Routes";
 
-const store = createStore(reducers, {}, applyMiddleware(thunk));
+const store = createStore(reducers, window.INITIAL_STATE, applyMiddleware(thunk));
 
 ReactDOM.hydrate(
     <Provider store={store}>
--- src/index.js
+++ src/index.js
@@ -16,8 +16,6 @@ app.get('*', (req, res) => {
         return route.loadData ? route.loadData(store) : null
     })
 
-    console.log(promises);
-
     Promise.all(promises).then(() => {
 
         res.send(renderer(req, store))
